8eae56cc4ca5a73b06f73d6821c236f4209806db
Takahiro Kato <takahiro.uk.brtn@gmail.com>
2020-06-10 22:52:53 +0000

    add validation and status column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -53,9 +53,10 @@ class TasksController < ApplicationController
  
   
   private
+  
     
     def task_params
-        params.require(:task).permit(:content)
+        params.require(:task).permit(:content,:status)
     end
     
 end
