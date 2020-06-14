8741471394353d832a0b49a7c10b48f8ec0b7d8b
Takahiro Kato <takahiro.uk.brtn@gmail.com>
2020-06-14 11:29:10 +0000

    reset db

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_223007) do
+ActiveRecord::Schema.define(version: 2020_06_14_112709) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
     t.string "status"
   end
 
